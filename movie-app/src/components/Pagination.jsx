ad6bcdf1f84a7f2a5f5a052e1ebf96a94e142cc6
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-07-03 21:32:50 +0300

    pagination

--- movie-app/src/components/Pagination.jsx
+++ movie-app/src/components/Pagination.jsx
@@ -1,46 +1,46 @@
-
 import { useEffect, useState } from 'react';
-// import { Pagination } from 'react-bootstrap'
 import ReactPaginate from 'react-paginate';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPage } from '../redux/actions/movieAction';
-import './pagination.css'; // 
-const PaginationComponent = () => {
-    const [pageCount, setpageCount] = useState(0)
+import './pagination.css';
 
-    const dispatch = useDispatch()
-    const pages = useSelector((state) => state.pageCount)
+const PaginationComponent = () => {
+    const [pageCount, setPageCount] = useState(0);
+
+    const dispatch = useDispatch();
+    const pages = useSelector((state) => state.pageCount);
 
     useEffect(() => {
-
-        setpageCount(pages)
-    }, [])
+        setPageCount(pages);
+    }, [pages]);
 
     const handlePageClick = (data) => {
+        dispatch(getPage(data.selected + 1));
+    };
 
-        dispatch(getPage(data.selected + 1))
-    }
     return (
-        <ReactPaginate
-            breakLabel="..."
-            nextLabel="التالى"
-            onPageChange={handlePageClick}
-            marginPagesDisplayed={3}
-            pageRangeDisplayed={2}
-            pageCount={500}
-            previousLabel="السابق"
-            containerClassName={"pagination justify-content-center p-3"}
-            pageClassName={"page-item"}
-            pageLinkClassName={"page-link"}
-            previousClassName={"page-item"}
-            nextClassName={"page-ite"}
-            previousLinkClassName={"page-link"}
-            nextLinkClassName={"page-link"}
-            breakClassName={"page-item"}
-            breakLinkClassName={"page-link"}
-            activeClassName={"active"}
-        />
+        <div className="d-flex justify-content-center p-3">
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="التالى"
+                onPageChange={handlePageClick}
+                marginPagesDisplayed={3}
+                pageRangeDisplayed={2}
+                pageCount={pageCount || 500}
+                previousLabel="السابق"
+                containerClassName={"pagination justify-content-center"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                previousClassName={"page-item"}
+                nextClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextLinkClassName={"page-link"}
+                breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                activeClassName={"active"}
+            />
+        </div>
     );
-}
+};
 
-export default PaginationComponent
+export default PaginationComponent;
